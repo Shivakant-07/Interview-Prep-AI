d91b1e0f9f42632ddaa76b8e7c1820c434a785e3
Shivakant Yadav <iamshivakantyadav@gmail.com>
2025-09-26 02:42:10 +0530

    server commit

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // Base URL of the backend API
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://interview-prep-ai-backend-0ltm.onrender.com";
 
 export const API_PATHS = {
     AUTH: {
